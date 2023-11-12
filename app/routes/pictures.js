94dd01d15eb7c87172f487cc84f4472724c5bbcd
MariKri123 <mari_kristiansen@live.com>
2023-11-12 15:54:26 +0100

    bug

--- app/routes/pictures.js
+++ app/routes/pictures.js
@@ -31,7 +31,7 @@ router.get('/',  requiresAuth(), async function(req, res, next) {
 router.get('/:pictureName',  requiresAuth(), async function(req, res, next) {
     let my_file = await s3.getObject({
         Bucket: process.env.CYCLIC_BUCKET_NAME,
-        Key: req.oidc.user.email + req.params.pictureName,
+        Key: req.oidc.user.email + '/' + req.params.pictureName,
     }).promise();
     const picture = {
         src: Buffer.from(my_file.Body).toString('base64'),
